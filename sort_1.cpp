196933895099a520cdf3100d7b60c86057dd54b2
Park Seungri <21900282@handong.edu>
2020-05-26 16:02:11 +0900

    Update sort_1.cpp - add record class

--- sort_1.cpp
+++ sort_1.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+class s_record{
+public :
+    string s_id;
+    string name;
+    double score;
+    s_record();
+    s_record(string s1, string s2, double n);
+};
+
 int main(){
     s_record s_list[12] = { {"21900013", "Kim ", 6.5}, {"21900136", "Lee ", 8.8 }, {"21900333", "Park", 9.2 }, { "21800442", "Choi", 7.1}, {"21900375", "Ryu ", 5.4  }, {"21700248", "Cho ", 6.3 }, { "21700302", "Jung", 8.3}, {"21800255", "Han ", 6.9  }, {"21800369", "Kang", 6.3 }, { "21900401", "Yang", 9.1}, {"21800123", "Moon", 8.1  }, {"21700678", "Seo ", 7.9 } };
     int n = 12;
