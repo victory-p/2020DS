ca4d87c8e17c3c35d7cf319ebed69fad8eb6da53
Park Seungri <21900282@handong.edu>
2020-05-26 16:07:46 +0900

    Update sort_1.cpp - insertion_sort function

--- sort_1.cpp
+++ sort_1.cpp
@@ -10,6 +10,19 @@ public :
     s_record(string s1, string s2, double n);
 };
 
+void insertion_sort(s_record a[], int n){
+    for(int i=1; i<n; i++){
+        s_record temp = a[i];
+        int j = i-1;
+
+        while((j>=0) && (a[j].s_id > temp.s_id)){
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = temp;
+    }
+}
+
 int main(){
     s_record s_list[12] = { {"21900013", "Kim ", 6.5}, {"21900136", "Lee ", 8.8 }, {"21900333", "Park", 9.2 }, { "21800442", "Choi", 7.1}, {"21900375", "Ryu ", 5.4  }, {"21700248", "Cho ", 6.3 }, { "21700302", "Jung", 8.3}, {"21800255", "Han ", 6.9  }, {"21800369", "Kang", 6.3 }, { "21900401", "Yang", 9.1}, {"21800123", "Moon", 8.1  }, {"21700678", "Seo ", 7.9 } };
     int n = 12;
