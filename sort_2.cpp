bde042c4969d2450e0efdeb3ef265b38b810c537
Park Seungri <21900282@handong.edu>
2020-06-08 20:43:48 +0900

    Update sort_2.cpp - record class

--- sort_2.cpp
+++ sort_2.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #define S_SIZE 50
 using namespace std;
+
+class s_record{
+public :
+    string s_id;
+    string name;
+    double score;
+    s_record();
+    s_record(string s1, string s2, double n);
+};
