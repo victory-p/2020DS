2bcd7783e02305545a5a31acdfd85f9a1a803729
Park Seungri <21900282@handong.edu>
2020-06-09 14:08:52 +0900

    Update sort_2.cpp - quicksort function

--- sort_2.cpp
+++ sort_2.cpp
@@ -22,3 +22,25 @@ s_record :: s_record(string s1, string s2, double n){
     name = s2;
     score = n;
 }
+
+void quick_sort(s_record a[], int left, int right){
+    s_record pivot, temp;
+    int i,j;
+    if(left >= right) return;
+    i = left;
+    j = right + 1;
+    pivot = a[left];
+
+    do{
+        do{
+            i++;
+        }while((i <= right) && (a[i].s_id < pivot.s_id));
+        do{
+            j--;
+        }while(a[j].s_id > pivot.s_id);
+        if(i<j) swap(a[i], a[j]);
+    }while(i<j);
+    swap(a[left], a[j]);
+    quick_sort(a, left, j-1);
+    quick_sort(a, j+1, right);
+}
