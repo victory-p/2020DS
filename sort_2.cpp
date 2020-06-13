f9694ede08739507ffd1a253691e2d13663bc376
Park Seungri <21900282@handong.edu>
2020-06-13 15:13:43 +0900

    Update sort_2.cpp - main

--- sort_2.cpp
+++ sort_2.cpp
@@ -139,3 +139,17 @@ void show_thelist(s_record a[], int n){
         cout << a[i].s_id << " : " << a[i].name << " : " << a[i].score << endl;
     }
 }
+
+int main(){
+    s_record s_list[12] = {{"21900013", "Kim", 6.5}, {"21900136", "Lee", 8.8}, {"21900333", "Park", 9.2}, {"21800442", "Choi", 7.1}, {"21900375", "Ryu", 5.4}, {"21700248", "Cho", 6.3}, {"21700302", "Jung", 8.3}, {"21800255", "Han", 6.9}, {"21800369", "Kang", 6.3}, {"21900401", "Yang", 9.1}, {"21800123", "Moon", 8.1}, {"21700678", "Seo", 7.9}};
+    int n = 12;     
+
+    quick_sort(s_list, 0, n-1);
+    heap_sort(s_list, n);
+    merge_sort(s_list, n); 
+
+    cout << "< The result of the sorting >" << endl;
+    show_thelist(s_list, n);
+
+    return 0;
+}
